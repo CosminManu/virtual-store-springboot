e9e6972fd4acc432b19e2a8fac4f7d417df4065a
CosminManu <cosmin.manu2000@gmail.com>
2025-02-24 17:29:13 +0200

    feature: finished OrderViewController

--- src/main/java/com/mcm/virtualstore/controller/OrderViewController.java
+++ src/main/java/com/mcm/virtualstore/controller/OrderViewController.java
@@ -5,8 +5,7 @@ import com.mcm.virtualstore.model.Order;
 import com.mcm.virtualstore.service.OrderService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,9 +20,26 @@ public class OrderViewController {
     @GetMapping                                             //home page
     public String viewOrders(Model model){
         List<Order> orders = orderService.getAllOrders();   //fetch all orders
-        model.addAttribute("orders");
+        model.addAttribute("orders", orders);
         model.addAttribute("statuses", OrderStatus.values());
         return "orders";                                    //returns thymeleaf template 'orders.html'
+    }
+
+    @PostMapping("/orders")
+    public String createOrder(@RequestParam String customerName){
+        orderService.createOrder(customerName);
+        return "redirect:/";        //redirects back to '/' (refresh page)
+    }
+    @PostMapping("/orders/{id}/status")
+    public String updateOrderStatus(@PathVariable Long id, @RequestParam OrderStatus orderStatus){
+        orderService.updateOrderStatus(id, orderStatus);
+        return "redirect:/";        //redirects back to '/' (refresh page)
+    }
+
+    @PostMapping("/orders/{id}")
+    public String deleteOrder(@PathVariable Long id){
+        orderService.deleteOrder(id);
+        return "redirect:/";        //redirects back to '/' (refresh page)
     }
 
 
